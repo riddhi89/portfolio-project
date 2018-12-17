35644efc27c7e1cfd851e344ea294f7af05dc504
Riddhi Shah <ridhi.j.shah@gmail.com>
2018-12-16 19:18:22 -0800

    Blog functionality and polish

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from jobs import views as job_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', job_views.home, name='home')
+    path('', job_views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
